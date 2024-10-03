930b59d64aa124bc8ce0f63b150c4103ec4fd306
Liam Horne <liam@liamhorne.com>
2024-10-03 17:54:19 -0400

    testing deployment with github ui commit

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 // Used when inserting text into a slide that cannot be empty
 // (sometimes Google Apps Script will complain if a box is empty
 export const EMPTY_STRING = "‎";
+export const EXAMPLE_DEPLOYMENT = "1";
 export const TEXT_COLOR_PRIMARY = "#595959";
 export const TEXT_COLOR_SECONDARY = "#999999";
 export const DEFAULT_AVATAR_URL =
